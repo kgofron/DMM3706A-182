d5802a8a76cd6c1e325e77d572e5477c0a9a69cc
kgofron <kgofron@bnl.gov>
2015-08-21 16:46:02 -0400

    [IXS] Keithley 2000 DMM ioc created

---
+++ iocBoot/iocDMM2k/st.cmd
@@ -0,0 +1,59 @@
+#!../../bin/linux-x86_64/DMM2k
+
+## You may have to change DMM2k to something else
+## everywhere it appears in this file
+
+< envPaths
+
+epicsEnvSet("ENGINEER",  "kgofron x5283")
+epicsEnvSet("LOCATION",  "740 IXS RG:D1")
+#epicsEnvSet("STREAM_PROTOCOL_PATH", ".:../protocols:$(PMACUTIL)/protocol")
+epicsEnvSet("EPICS_CA_AUTO_ADDR_LIST", "NO")
+epicsEnvSet("EPICS_CA_ADDR_LIST", "10.10.0.255")
+
+epicsEnvSet("P",         "XF:10IDD-BI")
+epicsEnvSet("R",         "{DMM:1-K2000}")
+# epicsEnvSet("IOCNAME",   "k2000")
+
+cd ${TOP}
+
+## Register all support components
+dbLoadDatabase "dbd/DMM2k.dbd"
+DMM2k_registerRecordDeviceDriver pdbbase
+
+############ Asyn Communication Config ############
+# cfg comms for Keithley 2000 controllers
+drvAsynIPPortConfigure("tsrv2-P3","10.10.2.54:4003")
+#drvAsynIPPortConfigure("tsrv2-P4","10.10.2.54:4004")
+
+## Load record instances
+#dbLoadTemplate "db/userHost.substitutions"
+#dbLoadRecords "db/dbSubExample.db", "user=kgofronHost"
+
+# Keithley 2000 DMM
+#dbLoadRecords("$(IP)/ipApp/Db/Keithley2kDMM_mf.db","P=xxx:,Dmm=D1,PORT=serial1")
+dbLoadRecords("/usr/lib/epics/db/Keithley2kDMM_mf.db","P=$(P),Dmm=$(R),PORT=tsrv2-P3")
+#dbLoadRecords("/usr/lib/epics/db/Keithley2kDMM_mf.db","P=$(P),Dmm=$(R),PORT=tsrv2-P4")
+
+### Load asynRecords for general comms to each PORT
+#dbLoadRecords("db/asyn.db")
+
+### devIocStats
+#dbLoadRecords("${EPICS_BASE}/db/iocAdminSoft.db", "IOC=XF:10IDB-CT{DMM:1}")
+###################################################
+
+## Set this to see messages from mySub
+#var mySubDebug 1
+
+## Run this to trace the stages of iocInit
+#traceIocInit
+
+cd ${TOP}/iocBoot/${IOC}
+iocInit
+
+## Start any sequence programs
+#seq sncExample, "user=kgofronHost"
+
+#seq &Keithley2kDMM, "P=13Keithley1:, Dmm=DMM1, channels=22, model=2700, stack=10000"
+#doAfterIocInit()
+seq &Keithley2kDMM, "P=$(P), Dmm=$(R), channels=1, model=2000"
